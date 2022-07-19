4f73b5a52e5e91541c598c2a1dc896e6c5c7762c
PIYUSH CHOUDHARY <71705154+Developer-piyush@users.noreply.github.com>
2022-07-19 13:46:35 +0530

    Update dark-mode.js

--- site/dark-mode/dark-mode.js
+++ site/dark-mode/dark-mode.js
@@ -8,6 +8,7 @@ if(localStorage.getItem("data-theme")){
 
 function toggleDark(r) {//this function is executed when switching from the current theme to the other
     const dataTheme = container.getAttribute("data-theme");
+    var style = document.createElement('style');
     let theme_switch;
     if(dataTheme === "light") {theme_switch = 1} else {theme_switch = 0}
     if(r){theme_switch = !theme_switch}//so the oppisite of the theme stored is used when calling this function 
@@ -16,23 +17,12 @@ function toggleDark(r) {//this function is executed when switching from the curr
         document.getElementById("night").style.display = "block";
         document.getElementById("light").style.display = "none";
         localStorage.setItem("data-theme", "dark");
+        
     } else {
         container.setAttribute("data-theme", "light");
         document.getElementById("night").style.display = "none";
         document.getElementById("light").style.display = "block";
         localStorage.setItem("data-theme", "light");
-    }
-    if (dataTheme === "dark") {
-        var style = document.createElement('style');
-        style.innerHTML = `
-        blockquote p, li{
-            color: #99fff5;
-        }
-        td, th{
-            color: #121111;
-        }
-        `;
-        document.head.appendChild(style);
     }
 }
 
@@ -45,3 +35,5 @@ function toggleDark(r) {//this function is executed when switching from the curr
 //     `;
 //     document.head.appendChild(style);
 // }
+
+
